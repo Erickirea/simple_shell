fb10f3b0ba27b30ae25315d11b08bf895e108c85
Erickirea <ericfoxe98@gmail.com>
2023-11-15 14:33:45 -0800

    simple shell code

--- shell.c
+++ shell.c
@@ -13,86 +13,50 @@ void display_prompt(void)
 }
 
 /**
- * process_input - Processes the user input
- * @input: User input
- * @count: Execution count
+ * main - Entry point to program
+ * @argc: Argument count
  * @argv: Argument vector
- * @stat: Exit status
+ * Return: Returns condition
  */
-void process_input(char *input, int count, char **argv, int *stat)
+int main(__attribute__((unused)) int argc, char **argv)
 {
-	char **commands, **cmd;
-	int i;
+	char *input, **cmd, **commands;
+	int count = 0, i, stat = 0;
 
-	if (input[0] == '\0')
-		return;
-
-	history(input);
-	commands = parser(input);
-
-	for (i = 0; commands[i] != NULL; i++)
-	{
-		cmd = parse_cmd(commands[i]);
-
-		if (_strcmp(cmd[0], "exit") == 0)
-		{
-			free(commands);
-			exit_bul(cmd, input, argv, count, *stat);
-		}
-		else if (check_builtin(cmd) == 0)
-		{
-			*stat = handle_builtin(cmd, *stat);
-			free(cmd);
-			continue;
-		}
-		else
-		{
-			*stat = find_cmd(cmd, input, count, argv);
-		}
-
-		free(cmd);
-	}
-
-	free(commands);
-}
-
-/**
- * run_shell - Main loop for the shell program
- * @argv: Argument vector
- * Return: Returns exit status
- */
-int run_shell(char **argv)
-{
-	char *input;
-	int count = 0, stat = 0;
-
+	if (argv[1] != NULL)
+		read_file(argv[1], argv);
 	signal(SIGINT, signal_to_handle);
-
 	while (1)
 	{
 		count++;
-
+		if (isatty(STDIN_FILENO))
 		display_prompt();
-		input = _getline();
-		process_input(input, count, argv, &stat);
-
-		free(input);
-		wait(&stat);
+	input = _getline();
+	if (input[0] == '\0')
+	continue;
+	history(input);
+	commands = parser(input);
+	for (i = 0; commands[i] != NULL; i++)
+	{
+	cmd = parse_cmd(commands[i]);
+	if (_strcmp(cmd[0], "exit") == 0)
+	{
+		free(commands);
+		exit_bul(cmd, input, argv, count, stat);
 	}
-
+		else if (check_builtin(cmd) == 0)
+	{
+		stat = handle_builtin(cmd, stat);
+		free(cmd);
+		continue;
+	}
+		else
+		stat = find_cmd(cmd, input, count, argv);
+		free(cmd);
+	}
+	free(input);
+	free(commands);
+	wait(&stat);
+	}
 	return (stat);
-}
-
-/**
- * main - Entry point to the shell program
- * @argc: Argument count
- * @argv: Argument vector
- * Return: Returns exit status
- */
-int main(int argc, char **argv)
-{
-	if (argc > 1)
-		read_file(argv[1], argv);
-
-	return (run_shell(argv));
 }
